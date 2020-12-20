2b472ad8b6a687d67cf73fb5c1dc3519d31dcb1d
ElisaEvans <75038445+ElisaEvans@users.noreply.github.com>
2020-12-20 13:54:26 +0000

    Add files via upload
    First iteration of a Best Picture Oscar winners game to practice creating a multiple choice quiz that provides a running % based on number of correct answers out of the total questions.

---
+++ Oscarsbestpicturewinners.py
@@ -0,0 +1,587 @@
+Q1 = """Which film won Best Picture in 1927/28?	
+A.	Wings	
+B.	7th Heaven	
+C.	The Racket	
+D.	""	
+E.	"" """	
+Q2 = """Which film won Best Picture in 1928/29?	
+A.	Alibi	
+B.	The Broadway Melody	
+C.	Hollwood Revue	
+D.	In Old Arizona	
+E.	The Patriot"""
+Q3 = """Which film won Best Picture in 1929/30?	
+A.	The Big House	
+B.	Disraeli	
+C.	The Divorcee	
+D.	All Quiet on the Western Front	
+E.	The Love Parade"""
+Q4 = """Which film won Best Picture in 1930/31?	
+A.	East Lynne	
+B.	The Front Page	
+C.	Cimarron	
+D.	Skippy	
+E.	Trader Horn"""
+Q5 = """Which film won Best Picture in 1931/32?	
+A.	Bad Girl	
+B.	The Champ	
+C.	Shanghai Express	
+D.	One Hour with You	
+E.	Grand Hotel"""
+Q6 = """Which film won Best Picture in 1932/33?	
+A.	42nd Street	
+B.	Cavalcade	
+C.	A Farewell to Arms	
+D.	The Private Life of Henry VIII	
+E.	She Done Him Wrong"""
+Q7 = """Which film won Best Picture in 1934?	
+A.	It Happened One Night	
+B.	Cleopatra	
+C.	The Gay Divorcee	
+D.	Imitation of Life	
+E.	Viva Villa!"""
+Q8 = """Which film won Best Picture in 1935?	
+A.	Alice Adams	
+B.	David Copperfield	
+C.	A Midsummer Night's Dream	
+D.	Top Hat	
+E.	Mutiny on the Bounty"""
+Q9 = """Which film won Best Picture in 1936?	
+A.	Dodsworth	
+B.	Romeo and Juliet	
+C.	The Great Ziefeld	
+D.	Mr. Deeds Goes to Town	
+E.	San Francisco"""
+Q10 = """Which film won Best Picture in 1937?	
+A.	The Awful Truth	
+B.	Dead End	
+C.	Stage Door	
+D.	The Life of Emile Zola	
+E.	A Star is Born"""
+Q11 = """Which film won Best Picture in 1938?	
+A.	You Can't Take It with You	
+B.	The Adventures of Robin Hood	
+C.	Boys Town	
+D.	Grand Illusion	
+E.	Pygmalion"""
+Q12 = """Which film won Best Picture in 1939?	
+A.	Of Mice and Men	
+B.	Gone with the Wind	
+C.	The Wizard of Oz	
+D.	Wuthering Heights	
+E.	Goodby, Mr. Chips"""
+Q13 = """Which film won Best Picture in 1940?	
+A.	The Grapes of Wrath	
+B.	The Great Dictator	
+C.	The Philadelphia Story	
+D.	The Letter	
+E.	Rebecca"""
+Q14 = """Which film won Best Picture in 1941?	
+A.	Citizen Kane	
+B.	The Maltese Falcon	
+C.	How Green Was My Valley	
+D.	Hold Back the Dawn	
+E.	Suspicion"""
+Q15 = """Which film won Best Picture in 1942?	
+A.	Mrs. Miniver	
+B.	The Pied Piper	
+C.	The Invaders	
+D.	Wake Island	
+E.	The Talk of the Town"""
+Q16 = """Which film won Best Picture in 1943?	
+A.	For Whom the Bell Tolls	
+B.	Watch on the Rhine	
+C.	Casablanca	
+D.	In Which We Serve	
+E.	Madame Curie"""
+Q17 = """Which film won Best Picture in 1944?	
+A.	Going My Way	
+B.	Gaslight	
+C.	Double Indemnity	
+D.	Since You Went Away	
+E.	Wilson"""
+Q18 = """Which film won Best Picture in 1945?	
+A.	Anchors Aweigh	
+B.	The Bells of St. Mary's	
+C.	The Lost Weekend	
+D.	Mildred Pierce	
+E.	Spellbound"""
+Q19 = """Which film won Best Picture in 1946?	
+A.	The Razor's Edge 	
+B.	Henry V	
+C.	It's a Wonderful Life	
+D.	The Best Years of Our Lives	
+E.	The Yearling"""
+Q20 = """Which film won Best Picture in 1947?	
+A.	The Bishop's Wife	
+B.	Crossfire	
+C.	Gentleman's Agreement	
+D.	Great Expectations	
+E.	Miracle on 34th Street"""
+Q21 = """Which film won Best Picture in 1948?	
+A.	Hamlet	
+B.	Johnny Belinda	
+C.	The Red Shoes	
+D.	The Snake Pit	
+E.	The Treasure of the Sierra Madre"""
+Q22 = """Which film won Best Picture in 1949?	
+A.	A Letter to Three Wives	
+B.	Battleground	
+C.	The Heiress	
+D.	All the King's Men	
+E.	Twelve O'Clock High"""
+Q23 = """Which film won Best Picture in 1950?	
+A.	All about Eve	
+B.	Born Yesterday	
+C.	Father of the Bride	
+D.	King Solomon's Mines	
+E.	Sunset Blvd."""
+Q24 = """Which film won Best Picture in 1951?	
+A.	An American in Paris	
+B.	Decision before Dawn	
+C.	A Place in the Sun	
+D.	Quo Vadis	
+E.	A Streetcar Named Desire"""
+Q25 = """Which film won Best Picture in 1952?	
+A.	Ivanhoe	
+B.	High Noon	
+C.	The Greatest Show on Earth	
+D.	Moulin Rouge	
+E.	The Quiet Man"""
+Q26 = """Which film won Best Picture in 1953?	
+A.	From Here to Eternity	
+B.	Julius Caesar	
+C.	The Robe	
+D.	Roman Holiday	
+E.	Shane"""
+Q27 = """Which film won Best Picture in 1954?	
+A.	The Caine Mutiny	
+B.	The Country Girl	
+C.	On the Waterfront	
+D.	Seven Brides for Seven Brothers	
+E.	Three Coins in the Fountain"""
+Q28 = """Which film won Best Picture in 1955?	
+A.	Love Is a Many-Splendored Thing	
+B.	Marty	
+C.	Mister Roberts	
+D.	Picnic	
+E.	The Rose Tattoo"""
+Q29 = """Which film won Best Picture in 1956?	
+A.	Around the World in 80 Days	
+B.	Friendly Persuasion	
+C.	Giant	
+D.	The King and I	
+E.	The Ten Commandments"""
+Q30 = """Which film won Best Picture in 1957?	
+A.	The Bridge on the River Kwai	
+B.	Peyton Place	
+C.	Sayonara	
+D.	12 Angry Men	
+E.	Witness for the Prosecution"""
+Q31 = """Which film won Best Picture in 1958?	
+A.	Auntie Mame	
+B.	Cat on a Hot Tin Roof	
+C.	The Defiant Ones	
+D.	Gigi	
+E.	Separate Tables"""
+Q32 = """Which film won Best Picture in 1959?	
+A.	Anatomy of a Murder	
+B.	Ben-Hur	
+C.	The Diary of Anne Frank	
+D.	The Nun's Story	
+E.	Room at the Top"""
+Q33 = """Which film won Best Picture in 1960?	
+A.	The Alamo	
+B.	The Apartment	
+C.	Elmer Gantry	
+D.	Sons and Lovers	
+E.	The Sundowners"""
+Q34 = """Which film won Best Picture in 1961?
+A.	Fanny	
+B.	The Guns of Navarone	
+C.	The Hustler	
+D.	Judgment at Nuremberg	
+E.	West Side Story"""
+Q35 = """Which film won Best Picture in 1962?	
+A.	Lawrence of Arabia	
+B.	The Longest Day	
+C.	Meredith Willson's The Music Man	
+D.	Mutiny on the Bounty	
+E.	To Kill a Mockingbird"""
+Q36 = """Which film won Best Picture in 1963?	
+A.	America America	
+B.	Cleopatra	
+C.	How the West Was Won	
+D.	Lilies of the Field	
+E.	Tom Jones"""
+Q37 = """Which film won Best Picture in 1964?	
+A.	Becket	
+B.	Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb	
+C.	Mary Poppins	
+D.	My Fair Lady	
+E.	Zorba the Greek"""
+Q38 = """Which film won Best Picture in 1965?	
+A.	Darling	
+B.	Doctor Zhivago	
+C.	Ship of Fools	
+D.	The Sound of Music	
+E.	A Thousand Clowns"""
+Q39 = """Which film won Best Picture in 1966?	
+A.	Alfie	
+B.	A Man for All Seasons	
+C.	The Russians Are Coming The Russians Are Coming	
+D.	The Sand Pebbles	
+E.	Who's Afraid of Virginia Woolf?"""
+Q40 = """Which film won Best Picture in 1967?	
+A.	Bonnie and Clyde	
+B.	Doctor Dolittle	
+C.	The Graduate	
+D.	Guess Who's Coming to Dinner	
+E.	In the Heat of the Night"""
+Q41 = """Which film won Best Picture in 1968?	
+A.	Funny Girl	
+B.	The Lion in Winter	
+C.	Oliver!	
+D.	Rachel, Rachel	
+E.	Romeo and Juliet"""
+Q42 = """Which film won Best Picture in 1969?	
+A.	Anne of the Thousand Days	
+B.	Butch Cassidy and the Sundance Kid	
+C.	Hello, Dolly!	
+D.	Midnight Cowboy	
+E.	Z"""
+Q43 = """Which film won Best Picture in 1970?	
+A.	Airport	
+B.	Five Easy Pieces	
+C.	Love Story	
+D.	M*A*S*H	
+E.	Patton"""
+Q44 = """Which film won Best Picture in 1971?	
+A.	A Clockwork Orange	
+B.	Fiddler on the Roof	
+C.	The French Connection	
+D.	The Last Picture Show	
+E.	Nicholas and Alexandra"""
+Q45 = """Which film won Best Picture in 1972?	
+A.	Cabaret	
+B.	Deliverance	
+C.	The Emigrants	
+D.	The Godfather	
+E.	Sounder"""
+Q46 = """Which film won Best Picture in 1973?	
+A.	American Graffiti	
+B.	Cries and Whispers	
+C.	The Exorcist	
+D.	The Sting	
+E.	A Touch of Class"""
+Q47 = """Which film won Best Picture in 1974?	
+A.	Chinatown	
+B.	The Conversation	
+C.	The Godfather Part II	
+D.	Lenny	
+E.	The Towering Inferno"""
+Q48 = """Which film won Best Picture in 1975?	
+A.	Barry Lyndon	
+B.	Dog Day Afternoon	
+C.	Jaws	
+D.	Nashville	
+E.	One Flew over the Cuckoo's Nest"""
+Q49 = """Which film won Best Picture in 1976?	
+A.	All the President's Men	
+B.	Bound for Glory	
+C.	Network	
+D.	Rocky	
+E.	Taxi Driver"""
+Q50 = """Which film won Best Picture in 1977?	
+A.	Annie Hall	
+B.	The Goodbye Girl	
+C.	Julia	
+D.	Star Wars	
+E.	The Turning Point"""
+Q51 = """Which film won Best Picture in 1978?	
+A.	Coming Home	
+B.	The Deer Hunter	
+C.	Heaven Can Wait	
+D.	Midnight Express	
+E.	An Unmarried Woman"""
+Q52 = """Which film won Best Picture in 1979?	
+A.	All That Jazz	
+B.	Apocalypse Now	
+C.	Breaking Away	
+D.	Kramer vs. Kramer	
+E.	Norma Rae"""
+Q53 = """Which film won Best Picture in 1980?	
+A.	Coal Miner's Daughter	
+B.	The Elephant Man	
+C.	Ordinary People	
+D.	Raging Bull	
+E.	Tess"""
+Q54 = """Which film won Best Picture in 1981?	
+A.	Atlantic City	
+B.	Chariots of Fire	
+C.	On Golden Pond	
+D.	Raiders of the Lost Ark	
+E.	Reds"""
+Q55 = """Which film won Best Picture in 1982?	
+A.	E.T. The Extra-Terrestrial	
+B.	Gandhi	
+C.	Missing	
+D.	Tootsie	
+E.	The Verdict"""
+Q56 = """Which film won Best Picture in 1983?	
+A.	The Big Chill	
+B.	The Dresser	
+C.	The Right Stuff	
+D.	Tender Mercies	
+E.	Terms of Endearment"""
+Q57 = """Which film won Best Picture in 1984?	
+A.	Amadeus	
+B.	The Killing Fields	
+C.	A Passage to India	
+D.	Places in the Heart	
+E.	A Soldier's Story"""
+Q58 = """Which film won Best Picture in 1985?	
+A.	The Color Purple	
+B.	Kiss of the Spider Woman	
+C.	Out of Africa	
+D.	Prizzi's Honor	
+E.	Witness"""
+Q59 = """Which film won Best Picture in 1986?	
+A.	Children of a Lesser God	
+B.	Hannah and Her Sisters	
+C.	The Mission	
+D.	Platoon	
+E.	A Room with a View"""
+Q60 = """Which film won Best Picture in 1987?	
+A.	Broadcast News	
+B.	Fatal Attraction	
+C.	Hope and Glory	
+D.	The Last Emperor	
+E.	Moonstruck"""
+Q61 = """Which film won Best Picture in 1988?	
+A.	The Accidental Tourist	
+B.	Dangerous Liaisons	
+C.	Mississippi Burning	
+D.	Rain Man	
+E.	Working Girl"""
+Q62 = """Which film won Best Picture in 1989?	
+A.	Born on the Fourth of July	
+B.	Dead Poets Society	
+C.	Driving Miss Daisy	
+D.	Field of Dreams	
+E.	My Left Foot"""
+Q63 = """Which film won Best Picture in 1990?	
+A.	Awakenings	
+B.	Dances With Wolves	
+C.	Ghost	
+D.	The Godfather, Part III	
+E.	Goodfellas"""
+Q64 = """Which film won Best Picture in 1991?	
+A.	Beauty and the Beast	
+B.	Bugsy	
+C.	JFK	
+D.	The Prince of Tides	
+E.	The Silence of the Lambs"""
+Q65 = """Which film won Best Picture in 1992?	
+A.	The Crying Game	
+B.	A Few Good Men	
+C.	Howards End	
+D.	Scent of a Woman	
+E.	Unforgiven"""
+Q66 = """Which film won Best Picture in 1993?	
+A.	The Fugitive	
+B.	In the Name of the Father	
+C.	The Piano	
+D.	The Remains of the Day	
+E.	Schindler's List"""
+Q67 = """Which film won Best Picture in 1994?	
+A.	Forrest Gump	
+B.	Four Weddings and a Funeral	
+C.	Pulp Fiction	
+D.	Quiz Show	
+E.	The Shawshank Redemption"""
+Q68 = """Which film won Best Picture in 1995?	
+A.	Apollo 13	
+B.	Babe	
+C.	Braveheart	
+D.	The Postman (Il Postino)	
+E.	Sense and Sensibility"""
+Q69 = """Which film won Best Picture in 1996?	
+A.	The English Patient	
+B.	Fargo	
+C.	Jerry Maguire	
+D.	Secrets & Lies	
+E.	Shine"""
+Q70 = """Which film won Best Picture in 1997?	
+A.	As Good as It Gets	
+B.	The Full Monty	
+C.	Good Will Hunting	
+D.	L.A. Confidential	
+E.	Titanic"""
+Q71 = """Which film won Best Picture in 1998?	
+A.	Elizabeth	
+B.	Life Is Beautiful	
+C.	Saving Private Ryan	
+D.	Shakespeare in Love	
+E.	The Thin Red Line"""
+Q72 = """Which film won Best Picture in 1999?	
+A.	American Beauty	
+B.	The Cider House Rules	
+C.	The Green Mile	
+D.	The Insider	
+E.	The Sixth Sense"""
+Q73 = """Which film won Best Picture in 2000?	
+A.	Chocolat	
+B.	Crouching Tiger, Hidden Dragon	
+C.	Erin Brockovich	
+D.	Gladiator	
+E.	Traffic"""
+Q74 = """Which film won Best Picture in 2001?	
+A.	A Beautiful Mind	
+B.	Gosford Park	
+C.	In the Bedroom	
+D.	The Lord of the Rings: The Fellowship of the Ring	
+E.	Moulin Rouge"""
+Q75 = """Which film won Best Picture in 2002?	
+A.	Chicago	
+B.	Gangs of New York	
+C.	The Hours	
+D.	The Lord of the Rings: The Two Towers	
+E.	The Pianist"""
+Q76 = """Which film won Best Picture in 2003?	
+A.	The Lord of the Rings: The Return of the King	
+B.	Lost in Translation	
+C.	Master and Commander: The Far Side of the World	
+D.	Mystic River	
+E.	Seabiscuit"""
+Q77 = """Which film won Best Picture in 2004?	
+A.	The Aviator	
+B.	Finding Neverland	
+C.	Million Dollar Baby	
+D.	Ray	
+E.	Sideways"""
+Q78 = """Which film won Best Picture in 2005?	
+A.	Brokeback Mountain	
+B.	Capote	
+C.	Crash	
+D.	Good Night, and Good Luck.	
+E.	Munich"""
+Q79 = """Which film won Best Picture in 2006?	
+A.	Babel	
+B.	The Departed	
+C.	Letters from Iwo Jima	
+D.	Little Miss Sunshine	
+E.	The Queen"""
+Q80 = """Which film won Best Picture in 2007?	
+A.	Atonement	
+B.	Juno	
+C.	Michael Clayton	
+D.	No Country for Old Men	
+E.	There Will Be Blood"""
+Q81 = """Which film won Best Picture in 2008?	
+A.	The Curious Case of Benjamin Button	
+B.	Frost/Nixon	
+C.	Milk	
+D.	The Reader	
+E.	Slumdog Millionaire"""
+Q82 = """Which film won Best Picture in 2009?	
+A.	Avatar	
+B.	District 9	
+C.	An Education	
+D.	The Hurt Locker	
+E.	Inglourious Basterds"""
+Q83 = """Which film won Best Picture in 2010?	
+A.	Black Swan	
+B.	The Fighter	
+C.	Inception	
+D.	The King's Speech	
+E.	True Grit"""
+Q84 = """Which film won Best Picture in 2011?	
+A.	The Artist	
+B.	The Help	
+C.	Hugo	
+D.	Moneyball	
+E.	War Horse"""
+Q85 = """Which film won Best Picture in 2012?	
+A.	Argo	
+B.	Django Unchained	
+C.	Les Miserables	
+D.	Life of Pi	
+E.	Zero Dark Thirty"""
+Q86 = """Which film won Best Picture in 2013?	
+A.	Dallas Buyers Club	
+B.	Gravity	
+C.	Nebraska	
+D.	12 Years a Slave	
+E.	The Wolf of Wall Street"""
+Q87 = """Which film won Best Picture in 2014?	
+A.	Birdman or (The Unexpected Virtue of Ignorance)	
+B.	The Grand Budapest Hotel	
+C.	The Imitation Game	
+D.	The Theory of Everything	
+E.	Whiplash"""
+Q88 = """Which film won Best Picture in 2015?	
+A.	The Big Short	
+B.	Mad Max: Fury Road	
+C.	The Martian	
+D.	The Revenant	
+E.	Spotlight"""
+Q89 = """Which film won Best Picture in 2016?	
+A.	Hidden Figures	
+B.	La La Land	
+C.	Lion	
+D.	Manchester by the Sea	
+E.	Moonlight"""
+Q90 = """Which film won Best Picture in 2017?	
+A.	Dunkirk	
+B.	Get Out	
+C.	Lady Bird	
+D.	Phantom Thread	
+E.	The Shape of Water"""
+Q91 = """Which film won Best Picture in 2018?	
+A.	Green Book	
+B.	Black Panther	
+C.	Bohemian Rhapsody	
+D.	The Favourite	
+E.	A Star is Born"""
+Q92 = """Which film won Best Picture in 2019?	
+A.	Parasite	
+B.	The Irishman	
+C.	Joker	
+D.	Little Women	
+E.	1917"""
+questions = {Q1:"A", Q2: "B", Q3: "D", Q4: "C", Q5: "E", Q6: "B", Q7: "A", Q8: "E", Q9: "C", Q10: "D", Q11: "A", Q12: "B", Q13: "E", Q14: "C", Q15: "A", Q16: "C", Q17: "A", Q18: "C", Q19: "D", Q20: "C", Q21: "A", Q22: "D", Q23: "A", Q24: "A", Q25: "C", Q26: "A", Q27: "C", Q28: "B", Q29: "A", Q30: "A", Q31: "D", Q32: "B", Q33: "B", Q34: "E", Q35: "A", Q36: "E", Q37: "D", Q38: "D", Q39: "B", Q40: "E", Q41: "C", Q42: "D", Q43: "E", Q44: "C", Q45: "D", Q46: "D", Q47: "C", Q48: "E", Q49: "D", Q50: "A", Q51: "B", Q52: "D", Q53: "C", Q54: "B", Q55: "B", Q56: "E", Q57: "A", Q58: "C", Q59: "D", Q60: "D", Q61: "D", Q62: "C", Q63: "B", Q64: "E", Q65: "E", Q66: "E", Q67: "A", Q68: "C", Q69: "A", Q70: "E", Q71: "D", Q72: "A", Q73: "D", Q74: "A", Q75: "A", Q76: "A", Q77: "C", Q78: "C", Q79: "B", Q80: "D", Q81: "E", Q82: "D", Q83: "D", Q84: "A", Q85: "A", Q86: "D", Q87: "A", Q88: "E", Q89: "E", Q90: "E", Q91: "A", Q92: "A"}
+name = input("Please enter your name:")
+print("Hello",name,"(｡◕‿◕｡)'ﾉ'' - let's test your Best Picture Oscars knowledge!")
+score=0
+for i in questions:
+    print()
+    print(i)
+    ans = input("Please enter your answer (A/B/C/D/E) or Quit to finish the game:").lower().upper()
+    if ans==questions[i]: 
+        print("Well done ʕ•́ᴥ•̀ʔっ that’s the correct answer! You win 1 point.")
+        score = score+1
+        print("Current score is:", score,"out of 92")
+        quotient = score/92
+        percentage = quotient * 100
+        progress = ('{:.0f}%'.format(percentage))
+        print("You've answered",progress,"of the total questions correctly so far!")
+    elif ans==("Quit").lower().upper():
+        break
+    else:
+        print("Uh oh,  ¯\_(ツ)_/¯ sorry that’s the wrong answer, you score 0 points.")
+        print("The correct answer is:")
+        print(questions.get(i))
+        score = score-0
+        print("Current score is:", score,"out of 92")
+        quotient = score/92
+        percentage = quotient * 100
+        progress = ('{:.0f}%'.format(percentage))
+        print("You've answered",progress,"of the total questions correctly so far!")
+        print(chr(27) + "[2J")
+        print()
+print("You've reached the end! Your final score is:", score)
+quotient = score/92
+percentage = quotient * 100
+finalscore = ('{:.0f}%'.format(percentage))
+print("Overall you answered",finalscore,"correctly!( ´ ▽ ` )ﾉ")
